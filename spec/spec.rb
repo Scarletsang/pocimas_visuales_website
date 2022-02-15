15dc89b208584a03dd363f44d3e6c6b4f88a4e3a
Anthony Tsang <anthonytsang0226@gmail.com>
2022-02-16 00:32:28 +0800

    completed most of the backend, except for more custom parsing of markdown documents

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,16 +1,3 @@
-require 'kramdown/document'
-require_relative "../src/markdown_parser.rb"
-require_relative "../src/yaml_parser.rb"
+require_relative "../src/renderer.rb"
 
-MARKDOWN_FOLDER = "../uploads/markdown/"
-
-def renderMarkdown(file_name)
-	input_text = File.read(MARKDOWN_FOLDER + file_name)
-	options = {
-		:input => 'CustomKramdown',
-		:parse_block_html => true
-	}
-	Kramdown::Document.new(input_text, options).to_html
-end
-
-puts from_yaml(MARKDOWN_FOLDER + "config.yaml")
+puts Document.from_yaml(UPLOAD_FOLDER + "config.yaml").render
